37914dd545847b0d446911faed2cd02a39420106
james-lanser <94556025+james-lanser@users.noreply.github.com>
2024-04-13 18:32:35 -0500

    Spring 2024 new lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -21,3 +21,32 @@ lightbox.addEventListener('click', e => {
 	if (e.target !== e.currentTarget) return
 		lightbox.classList.remove('active')
 })
+
+
+
+
+
+/*
+const docs = document.querySelectorAll('iframe')
+docs.forEach(doc => {
+	doc.addEventListener('click', e => {
+		lightbox.classList.add('active')
+		const iframe = document.createElement('iframe')
+		iframe.src = doc.src
+		iframe.style.height = '70%'
+		iframe.style.width = 'auto'
+		while (lightbox.firstChild) {
+			lightbox.removeChild(lightbox.firstChild)
+		}
+		lightbox.appendChild(iframe)
+	})
+})
+
+lightbox.addEventListener('click', e => {
+	if (e.target !== e.currentTarget) return
+		lightbox.classList.remove('active')
+})
+*/
+
+
+
